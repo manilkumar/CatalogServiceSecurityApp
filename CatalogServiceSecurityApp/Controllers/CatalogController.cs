5e9a0f98b4007380f8f6e827c7635db10283a60a
Anilkumar Menta <Anilkumar_Menta@epam.com>
2023-12-13 13:55:08 +0530

    Add project files.

---
+++ CatalogServiceSecurityApp/Controllers/CatalogController.cs
@@ -0,0 +1,88 @@
+﻿namespace HRApplication.Controllers
+{
+    using CatalogServiceSecurityApp.Models.DbModels;
+    using CatalogServiceSecurityApp.Services;
+    using HRApplication.Models.DbModels;
+    using HRApplication.Models.InputModels;
+    using HRApplication.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : Controller
+    {
+        private readonly ILogger logger;
+        private readonly CategoryService categoryService;
+
+        public CatalogController(ILogger logger, CategoryService authService)
+        {
+            this.categoryService = categoryService;
+            this.logger = logger;
+        }
+
+        [Authorize(Roles = "Buyer,Manager")]
+        [HttpGet]
+        public ActionResult<List<Category>> Get()
+        {
+            try
+            {
+                var categories = this.categoryService.GetAll();
+                return Ok(categories);
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error.Message);
+                return StatusCode(500);
+            }
+        }
+
+        [Authorize(Roles = "Manager")]
+        [HttpPost]
+        public ActionResult Create([FromBody] Category category)
+        {
+            try
+            {
+                this.categoryService.CreateCateogry(category);
+                return Ok();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error.Message);
+                return StatusCode(500);
+            }
+        }
+
+        [Authorize(Roles = "Manager")]
+        [HttpPut]
+        public ActionResult Update([FromBody] Category category)
+        {
+            try
+            {
+                this.categoryService.UpdateCateogry(category);
+                return Ok();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error.Message);
+                return StatusCode(500);
+            }
+        }
+
+        [Authorize(Roles = "Manager")]
+        [HttpDelete]
+        public ActionResult Delete(int categoryId)
+        {
+            try
+            {
+                this.categoryService.DeleteCateogry(categoryId);
+                return Ok();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error.Message);
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ CatalogServiceSecurityApp/Services/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using CatalogServiceSecurityApp.Models.DbModels;
+using HRApplication.Data;
+
+namespace CatalogServiceSecurityApp.Services
+{
+    public class CategoryService
+    {
+        private readonly DataContext dataContext;
+
+        public CategoryService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public List<Category> GetAll()
+        {
+            return this.dataContext.Categories.ToList();
+        }
+        public void CreateCateogry(Category category)
+        {
+            dataContext.Categories.Add(category);
+            dataContext.SaveChanges();
+        }
+
+        public void UpdateCateogry(Category updateCategory)
+        {
+            var category = this.dataContext.Categories.Where(i => i.Id == updateCategory.Id).FirstOrDefault();
+            if (category != null)
+            {
+                category.Name = updateCategory.Name;
+                category.ImageURL = updateCategory.ImageURL;
+            }
+            dataContext.SaveChanges();
+        }
+
+        public void DeleteCateogry(int categoryId)
+        {
+            var category = this.dataContext.Categories.Where(i => i.Id == categoryId).FirstOrDefault();
+            if (category != null)
+            {
+                dataContext.Remove(category);
+            }
+            dataContext.SaveChanges();
+        }
+    }
+}
